eeabad7b06a47f09c8ad2223744e49d55960605d
paprika-mah <paprika.mah@gmail.com>
2020-05-12 16:33:51 +0900

    chk

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,14 +1,13 @@
 class MembersController < ApplicationController
-  # before_action :login_required
+  before_action :login_required
 
-  # # 会員一覧
+  # 会員一覧
   def index
     @members = Member.order("number")
       .page(params[:page]).per(15)
-    # @members = Member.order("number")
   end
 
-  # # 検索
+  # 検索
   def search
     @members = Member.search(params[:q])
       .page(params[:page]).per(15)
@@ -26,25 +25,25 @@ class MembersController < ApplicationController
     @member = Member.new(birthday: Date.new(1980, 1, 1))
   end
 
-  # 編集フォーム
+  # 更新フォーム
   def edit
     @member = Member.find(params[:id])
   end
 
-
   # 会員の新規登録
   def create
-    @member = Member.new(params[:member])
+    @member = Member.new(member_params)
     if @member.save
       redirect_to @member, notice: "会員を登録しました。"
     else
       render "new"
     end
   end
+
   # 会員情報の更新
   def update
     @member = Member.find(params[:id])
-    @member.assign_attributes(params[:member])
+    @member.assign_attributes(member_params)
     if @member.save
       redirect_to @member, notice: "会員情報を更新しました。"
     else
@@ -57,5 +56,22 @@ class MembersController < ApplicationController
     @member = Member.find(params[:id])
     @member.destroy
     redirect_to :members, notice: "会員を削除しました。"
+  end
+
+  # ストロング・パラメータ
+  private def member_params
+    attrs = [
+      :number,
+      :name,
+      :full_name,
+      :gender,
+      :birthday,
+      :email,
+      :administrator
+    ]
+
+    attrs << :password if action_name == "create"
+
+    params.require(:member).permit(attrs)
   end
 end
